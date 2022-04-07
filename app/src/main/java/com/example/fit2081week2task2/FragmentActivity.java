2489e5a4b47474704f5c766a83e31e2a6a636109
StealthyMage <36330911+StealthyMage@users.noreply.github.com>
2022-04-07 11:37:25 +1000

    Round 3 bois

--- app/src/main/java/com/example/fit2081week2task2/FragmentActivity.java
+++ app/src/main/java/com/example/fit2081week2task2/FragmentActivity.java
@@ -1,4 +1,13 @@
 package com.example.fit2081week2task2;
 
-public class FragmentActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FragmentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_activity);
+    }
 }
