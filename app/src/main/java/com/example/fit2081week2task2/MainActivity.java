6b553d1593d93de2f2cf95bd00ec84ddb7cc3f6c
StealthyMage <36330911+StealthyMage@users.noreply.github.com>
2022-05-11 13:12:37 +1000

    Week 10 onTouchEvent

--- app/src/main/java/com/example/fit2081week2task2/MainActivity.java
+++ app/src/main/java/com/example/fit2081week2task2/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -56,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
     //Week 8 Firebase
     FirebaseDatabase mFBDB;
     DatabaseReference ref;
+    //Week 10 Velocity Stuff
+    private VelocityTracker mVelocityTracker = null;
 
 
     @Override
@@ -330,4 +334,53 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         }
     };
+
+    //Week 10 onTouchEvent
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                if(mVelocityTracker == null){
+                    mVelocityTracker = VelocityTracker.obtain();
+                }
+                else{
+                    mVelocityTracker.clear();
+                }
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker.addMovement(event);
+                mVelocityTracker.computeCurrentVelocity(1000);
+                int newX = (int)mVelocityTracker.getXVelocity(pointerId);
+                int newY = (int)mVelocityTracker.getYVelocity(pointerId);
+                if((newX - x) > 50){
+                    addListItem();
+                    MovieDetails newMovie = new MovieDetails(mMovieName.getText().toString(), mMovieYear.getText().toString(), mMovieCountry.getText().toString(), mMovieCost.getText().toString(), mMovieGenre.getText().toString(), mMovieKeywords.getText().toString());
+                    mMovieViewModel.insert(newMovie);
+                    ref.push().setValue(newMovie);
+                    adapter.notifyDataSetChanged();
+                }
+                else if((newY-y) > 50){
+                    while(0 < mMovieArray.size()){
+                        mMovieArray.remove(mMovieArray.size()-1);
+                        adapter.notifyDataSetChanged();
+                    }
+                    //Deletes all of the entries in the Database.
+                    mMovieViewModel.deleteAll();
+                    ref.setValue(null);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mVelocityTracker.recycle();
+                break;
+        }
+    return true;
+    }
+
 }
